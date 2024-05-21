41d62492fc1a96391318b6ef2eaabddf45f1a64a
suelenadles <suelenadles@hotmail.com>
2024-05-21 16:41:55 -0300

    first commit

---
+++ backend-loja/src/main/java/com/magos/backendloja/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.magos.backendloja.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Olá mundo!!!!" + new Date();
+    }
+}
